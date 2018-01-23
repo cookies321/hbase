76b3666bbcbc98772d6c6aa30a77e558cd297dd3
robert <ss>
2018-01-23 15:57:48 +0800

    init project。

---
+++ src/main/java/com/holyrobot/rsdataprovider/RsdataproviderApplication.java
@@ -0,0 +1,12 @@
+package com.holyrobot.rsdataprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsdataproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsdataproviderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/holyrobot/rsdataprovider/RsdataproviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.holyrobot.rsdataprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RsdataproviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
